9ad76bcc39f8c2992502155ff52895c4398a71d7
huatang92 <hua92601@gmail.com>
2014-10-23 18:25:20 -0400

    Oct.23

--- CoolMatrixUtility/src/cmu/decomp/svd/Slave.java
+++ CoolMatrixUtility/src/cmu/decomp/svd/Slave.java
@@ -1,15 +1,29 @@
 package cmu.decomp.svd;
 
+import cmu.core.Mat;
+
 /**
  * Slave Class is used to identify different slaves.
  * 
- * @param sNum 
- *           the number of a slave.
+ * @param SlaveId
+ *          id of Slave
+ * @param SlaveMatrix
+ * 			matrix after computing svd
+ * @param workspan
+ * 		    time between return and send 
  */
 public class Slave {
-	public int sNum;
+	public int SlaveId;
+	public Mat SlaveMatrix;
+	public double workspan = Double.MAX_VALUE;
 	
-	public Slave (int num) {
-		this.sNum = num;
+	public Slave (int SlaveId, Mat SlaveMatrix) {
+		this.SlaveId = SlaveId;
+		this.SlaveMatrix = SlaveMatrix;		
+	}
+	public Slave (int SlaveId, Mat SlaveMatrix, double workspan) {
+		this.SlaveId = SlaveId;
+		this.SlaveMatrix = SlaveMatrix;		
+		this.workspan = workspan;
 	}
 }
